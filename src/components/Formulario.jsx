74ca9eddfb197255f308b9560f2e34ea0a90da36
FacundoEstrada <facundoestrada1015@gmail.com>
2022-03-04 19:43:45 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover{
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+
+    const [SelectMonedas] = useSelectMonedas();
+
+    SelectMonedas();
+    return (
+        <form action="">
+            <InputSubmit type="submit" value="Cotizar" />
+        </form>
+    )
+}
+
+export default Formulario
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+
+    const SelectMonedas = () => {
+        console.log("Desde selector monedas");
+    }
+
+    return [SelectMonedas]
+}
+
+export default useSelectMonedas
